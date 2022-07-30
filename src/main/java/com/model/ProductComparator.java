cf232a86fc879c8531c6ac5f5c61f1eda9cd6b79
KanKori <Lanokapols73>
2022-07-30 06:37:08 +0300

    added ProductComparator

---
+++ src/main/java/com/model/ProductComparator.java
@@ -0,0 +1,16 @@
+package com.model;
+
+import java.util.Comparator;
+
+public class ProductComparator<T extends Product> implements Comparator<T> {
+    @Override
+    public int compare(T firstProduct, T secondProduct) {
+        if (secondProduct.getPrice() == firstProduct.getPrice()) {
+            if (firstProduct.getTitle().equals(secondProduct.getTitle())) {
+                return Integer.compare(firstProduct.getCount(), secondProduct.getCount());
+            }
+            return firstProduct.getTitle().compareTo(secondProduct.getTitle());
+        }
+        return Double.compare(secondProduct.getPrice(), firstProduct.getPrice());
+    }
+}

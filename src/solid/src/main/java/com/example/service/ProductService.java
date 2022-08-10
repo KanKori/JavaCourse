516eb31593096b95f1c30a975d1eb0afe52917b0
KanKori <Lanokapols73>
2022-08-10 15:59:11 +0300

    deleted instance

--- src/solid/src/main/java/com/example/service/ProductService.java
+++ src/solid/src/main/java/com/example/service/ProductService.java
@@ -5,16 +5,7 @@ import com.example.repository.IProductRepository;
 import com.example.repository.ProductRepository;
 
 public class ProductService extends AbstractProductService<Product> {
-    private static ProductService instance;
-
     public ProductService(ProductRepository repository) {
         super(repository);
-    }
-
-    public static ProductService getInstance() {
-        if (instance != null) {
-            instance = new ProductService(ProductRepository.getInstance());
-        }
-        return instance;
     }
 }

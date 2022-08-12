5555df9586df94c26b86c0ef433bb48888306d92
KanKori <Lanokapols73>
2022-08-13 00:06:06 +0300

    cosmetic parser fixes

--- src/module/src/main/java/com/service/Parser.java
+++ src/module/src/main/java/com/service/Parser.java
@@ -8,8 +8,6 @@ import org.slf4j.LoggerFactory;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;

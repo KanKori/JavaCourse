8eb51bb44eaa0ba92eb1f815d5a75b965d9ede64
KanKori <Lanokapols73>
2022-08-14 18:09:58 +0300

    updated StatisticsServiceTest

--- src/module/src/test/java/com/service/statistics/StatisticsServiceTest.java
+++ src/module/src/test/java/com/service/statistics/StatisticsServiceTest.java
@@ -1,40 +1,44 @@
 package com.service.statistics;
 
-import com.model.product.specifications.ProductType;
 import com.service.shop.ShopService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 
 class StatisticsServiceTest {
     private StatisticsService target;
-    private ShopService shopService;
 
     @BeforeEach
     void setUp() {
-        shopService = Mockito.mock(ShopService.class);
+        ShopService shopService = Mockito.mock(ShopService.class);
         target = new StatisticsService(shopService.getInvoiceList());
     }
 
     @Test
-    void productsCountByType() {
-        assertNotNull(target.productsCountByType(ProductType.TELEVISION));
+    void lowestSumInvoice_assertNotNull() {
+        assertNotNull(target.getLowestSumInvoice());
     }
 
     @Test
-    void amountOfRetail() {
-        assertNotNull(target.amountOfRetail());
+    void invoicesWithSingleProductType_assertNotNull() {
+        assertNotNull(target.getInvoicesWithSingleProductType());
     }
 
     @Test
-    void lowestSumInvoice() {
-        assertNotNull(target.lowestSumInvoice());
+    void firstThreeInvoices_assertNotNull() {
+        assertNotNull(target.getFirstThreeInvoices());
     }
 
     @Test
-    void sumAllInvoices() {
-        assertNotNull(target.sumAllInvoices());
+    void invoicesByPersonsUnderAdultAge_assertNotNull() {
+        assertNotNull(target.getInvoicesByPersonsUnderAdultAge());
+    }
+
+    @Test
+    void sortedInvoices_assertNotNull() {
+        assertNotNull(target.getSortedInvoices());
     }
 }

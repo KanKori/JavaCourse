36ed9f710320478cf1f686308f9f3801a121cf60
KanKori <Lanokapols73>
2022-08-14 03:01:00 +0300

    fixed ParserCSV

--- src/module/src/main/java/com/service/parser/ParserCSV.java
+++ src/module/src/main/java/com/service/parser/ParserCSV.java
@@ -45,33 +45,6 @@ public class ParserCSV {
         return lines;
     }
 
-    protected List<String> readLinesFromCSV() {
-        String line;
-        List<String> lines = new ArrayList<>();
-        String csv = ".\\src\\module\\src\\main\\resources\\products.csv";
-        BufferedReader bufferedReader;
-        Pattern lineCSV = Pattern.compile("^((?:[^,]+,\\s*){6})[^,]+");
-        try {
-            bufferedReader = new BufferedReader(new FileReader(csv));
-            while ((line = bufferedReader.readLine()) != null) {
-                Matcher matcher = lineCSV.matcher(line);
-                if (matcher.find()) {
-                    lines.add(line);
-                } else {
-                    try {
-                        throw new InvalidLineException(toString());
-                    } catch (InvalidLineException e) {
-                        LOGGER.error(e.getMessage());
-                    }
-                }
-            }
-        } catch (IOException e) {
-            LOGGER.error(e.getMessage());
-        }
-        return lines;
-    }
-
-
     public List<AbstractProduct> parseCSV(String csv) throws InvalidLineException {
         final int STRING_WITH_NAMES_OF_COLUMN = 0;
         String[] columns = readLinesFromCSV(csv).get(STRING_WITH_NAMES_OF_COLUMN).split(",");
@@ -81,27 +54,6 @@ public class ParserCSV {
         List<AbstractProduct> products = new ArrayList<>();
         Map<String, String> result = new HashMap<>();
         List<String> values = readLinesFromCSV(csv);
-        values.stream()
-                .skip(LINE_WITH_COLUMNS)
-                .map(splitChar -> splitChar.split(","))
-                .forEach(value -> {
-                    for (int i = FIRST_SPECIFICATION; i < value.length; i++) {
-                        result.put(columns[i], value[i]);
-                    }
-                    products.add(ProductFactory.createProductFromMap(result));
-                });
-        return products;
-    }
-
-    public List<AbstractProduct> parseCSV() {
-        final int STRING_WITH_NAMES_OF_COLUMN = 0;
-        String[] columns = readLinesFromCSV().get(STRING_WITH_NAMES_OF_COLUMN).split(",");
-
-        final int FIRST_SPECIFICATION = 0;
-        final int LINE_WITH_COLUMNS = 1;
-        List<AbstractProduct> products = new ArrayList<>();
-        Map<String, String> result = new HashMap<>();
-        List<String> values = readLinesFromCSV();
         values.stream()
                 .skip(LINE_WITH_COLUMNS)
                 .map(splitChar -> splitChar.split(","))

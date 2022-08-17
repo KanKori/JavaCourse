db915a6fa84ac588b1d7e53a6cb1e31c3d09ba5f
KanKori <inavoinavo@gmail.com>
2022-08-17 05:13:02 +0300

    hw 23: added JDBConfig

---
+++ src/main/java/com/config/JDBCConfig.java
@@ -0,0 +1,20 @@
+package com.config;
+
+import lombok.SneakyThrows;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class JDBCConfig {
+    private static final String URL = "jdbc:postgresql://localhost:5432/test";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "root";
+
+    private JDBCConfig() {
+    }
+
+    @SneakyThrows
+    public static Connection getConnection() {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}

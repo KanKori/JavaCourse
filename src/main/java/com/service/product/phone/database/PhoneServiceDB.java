8caa5883adaeca9a2170048597937a1b5328a24d
KanKori <inavoinavo@gmail.com>
2022-08-17 09:10:25 +0300

    code revision: cleared PhoneServiceDB

--- src/main/java/com/service/product/phone/database/PhoneServiceDB.java
+++ src/main/java/com/service/product/phone/database/PhoneServiceDB.java
@@ -28,19 +28,6 @@ public class PhoneServiceDB extends ProductService<Phone> {
         return instance;
     }
 
-    public static Phone createPhoneFromMap(Map<String, String> phoneMap) {
-        return new Phone(
-                phoneMap.getOrDefault("title", "DefaultTitle"),
-                phoneMap.getOrDefault("model", "DefaultModel"),
-                Double.parseDouble(phoneMap.getOrDefault("price", String.valueOf(0))),
-                phoneMap.get("currency"),
-                PhoneManufacturer.valueOf(phoneMap.getOrDefault("manufacturer", PhoneManufacturer.SAMSUNG.name())),
-                LocalDateTime.parse(phoneMap.get("created"), DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'")),
-                Integer.parseInt(phoneMap.get("count")),
-                new OperatingSystem(phoneMap.get("designation"), Integer.parseInt(phoneMap.get("version")))
-        );
-    }
-
     public Phone createProduct() {
         return new Phone(
                 "Title-" + RANDOM.nextInt(1000),
@@ -54,12 +41,5 @@ public class PhoneServiceDB extends ProductService<Phone> {
         final PhoneManufacturer[] values = PhoneManufacturer.values();
         final int index = RANDOM.nextInt(values.length);
         return values[index];
-    }
-
-    public void deletePhoneFindByIdIfManufacturerApple(String id) {
-        getRepository().findById(id)
-                .filter(checkingPhone -> checkingPhone.getPhoneManufacturer().equals(PhoneManufacturer.APPLE))
-                .ifPresentOrElse(checkedPhone -> getRepository().delete(checkedPhone.getId()),
-                        () -> System.out.println("no one Apple Phone founded"));
     }
 }

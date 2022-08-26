91ddf66cceed29e32fc0be12b8e687bfb2e73b8e
KanKori <inavoinavo@gmail.com>
2022-08-27 00:18:27 +0300

    code revision: some fix

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,15 +1,10 @@
 package com;
 
-import com.model.invoice.Invoice;
 import com.model.product.AbstractProduct;
 import com.repository.invoice.database.InvoiceRepositoryDB;
-import com.repository.product.database.laptop.LaptopRepositoryDB;
 import com.repository.product.database.phone.PhoneRepositoryDB;
-import com.repository.product.database.tablet.TabletRepositoryDB;
 import com.service.invoice.database.InvoiceServiceDB;
-import com.service.product.laptop.database.LaptopServiceDB;
 import com.service.product.phone.database.PhoneServiceDB;
-import com.service.product.tablet.database.TabletServiceDB;
 
 import java.util.ArrayList;
 import java.util.List;

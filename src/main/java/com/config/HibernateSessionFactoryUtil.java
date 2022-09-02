9cc5d41fb47c23a057ce05108ab34784519c6728
KanKori <inavoinavo@gmail.com>
2022-09-02 03:46:24 +0300

    code revision: some fix

--- src/main/java/com/config/HibernateSessionFactoryUtil.java
+++ src/main/java/com/config/HibernateSessionFactoryUtil.java
@@ -1,14 +1,18 @@
 package com.config;
 
+import com.repository.product.database.phone.PhoneRepositoryDB;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 
-public class HibernateConfig {
+public class HibernateSessionFactoryUtil {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PhoneRepositoryDB.class);
     private static SessionFactory sessionFactory;
     private Session session;
 
@@ -20,6 +24,7 @@ public class HibernateConfig {
             try {
                 sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
             } catch (Exception e) {
+                LOGGER.error(String.valueOf(e));
                 StandardServiceRegistryBuilder.destroy(registry);
                 throw e;
             }
